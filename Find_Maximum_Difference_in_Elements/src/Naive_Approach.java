a5564ac7d8d51c177a764be4cfcb694323c10ea6
MyGit465 <gangwaraman88.4@gmail.com>
2021-02-01 11:35:59 +0530

    Naive Approach o(n^2)

---
+++ Find_Maximum_Difference_in_Elements/src/Naive_Approach.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Naive_Approach {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		 System.out.println("Enter Size of array :");
+		 int n =sc.nextInt();
+		 int arr []=new int[n];
+		 System.out.print("Eneter The Element of an array :");
+		 for(int i=0; i<n; i++) {
+			 arr[i]=sc.nextInt();
+		 }
+		 int max=0;
+		 int res=0;
+		 for(int i=0; i<n ; i++) {
+			 for(int j=i+1; j<n; j++) {
+				 res=arr[j]-arr[i];
+				 if(res>max) {
+					 max=res;
+				 }
+			 }
+		 }
+		 System.out.println(max);
+	}
+
+}
