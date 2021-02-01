f0cbc646d01315d13be96007fd04d848c86ec0ba
MyGit465 <gangwaraman88.4@gmail.com>
2021-02-01 11:07:13 +0530

    Using Efficient Approach

---
+++ Leader_In_array_Problem_Using_Efficient_approach/src/Efficient_approach.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Efficient_approach {
+
+	public static void main(String[] args) {
+		 Scanner sc=new Scanner(System.in);
+		 System.out.println("Enter Size of array :");
+		 int n =sc.nextInt();
+		 int arr []=new int[n];
+		 System.out.print("Eneter The Element of an array :");
+		 for(int i=0; i<n; i++) {
+			 arr[i]=sc.nextInt();
+		 }
+		  int leader = arr[n-1];
+		  for(int i=n-1; i>=0; i-- ) {
+			  if(arr[i] > arr[leader]) {
+				  leader = arr[i];
+				  System.out.println(arr[i]);
+			  }
+		  }
+	}
+
+}
